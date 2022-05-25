633954ae6fe15d0e8fdaf09266d4d29aae23c773
Stefano Casasola <ste.casasola@gmail.com>
2022-05-25 09:52:20 +0200

    Creates the component with an initial form

---
+++ src/components/ReservationForm.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+// name
+// phone
+// numberOfPeople
+// dateTime
+// smoking
+// specialRequests
+
+class ReservationForm extends Component {
+  render() {
+    return (
+      <div className="my-2 text-center">
+        <h2>Book your table here!</h2>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default ReservationForm
